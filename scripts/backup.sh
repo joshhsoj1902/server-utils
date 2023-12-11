763deda42c6c8e752c66c0dda3293a8b63a2bc7b
Josh <joshhsoj1902@gmail.com>
2023-12-11 18:40:50 -0500

    add weekly & monthly backups

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -11,12 +11,21 @@ crontab -l > ~/backups/crontab.backup
 # Backup fstab
 cp -p /etc/fstab ~/backups/fstab.backup
 
-
-# 7z is used here because it has better support for updating existing archives than tar.
-if [ -z "$1" ]
+opts=""
+if [ -n "$1" ]
 then
-    7z u ${BACKUP_DIR}/backup-$(date +%A).7z ~
-else
     #-p allows for a password to be set
-    7z -p"$1" u ${BACKUP_DIR}/backup-$(date +%A).7z ~
+    opts=$opts" -p${1}"
+fi
+
+7z $opts u ${BACKUP_DIR}/backup-$(date +%A).7z ~
+
+#Update weekly backup on Mondays
+if [[ $(date +%w) == 1 ]]; then
+    7z $opts u ${BACKUP_DIR}/backup-Weekly.7z ~
+fi
+
+#Update monthly
+if [[ $(date +%d) == 1 ]]; then
+    7z $opts u ${BACKUP_DIR}/backup-Monthly.7z ~
 fi
